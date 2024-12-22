5f70950f6a0dadaec76baebca21c6fe7956fe5c0
CerenayKuzu <cerenaykuzu2002@gmail.com>
2024-12-22 21:00:16 +0100

    Apache

--- Classes/Nurse.java
+++ Classes/Nurse.java
@@ -1,40 +1,61 @@
 package Classes;
 
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+
 public class Nurse {
     private String name;
     private int id;
-    private boolean isOnDuty; //the nurse is on duty or not
-
-    //Constructor
+    private boolean isOnDuty;
+    // Constructor
     public Nurse(int id, String name, boolean isOnDuty) {
         this.id = id;
         this.name = name;
         this.isOnDuty = isOnDuty;
+        logDebug("Nurse created with initial values.");
     }
 
-    //get - set
     public int getId() {
+        logDebug("getId() called. Returning: " + id);
         return id;
     }
 
     public void setId(int id) {
+        logDebug("setId() called. Old ID: " + this.id + ", New ID: " + id);
         this.id = id;
     }
 
     public String getName() {
+        logDebug("getName() called. Returning: " + name);
         return name;
     }
 
     public void setName(String name) {
+        logDebug("setName() called. Old Name: " + this.name + ", New Name: " + name);
         this.name = name;
     }
 
-    //check if the nurse is currently on duty
     public boolean isOnDuty() {
+        logDebug("isOnDuty() called. Returning: " + isOnDuty);
         return isOnDuty;
     }
-    //change the nurse's duty status
+
     public void setOnDuty(boolean onDuty) {
-        isOnDuty = onDuty;
+        logDebug("setOnDuty() called. Old Duty Status: " + this.isOnDuty + ", New Duty Status: " + onDuty);
+        this.isOnDuty = onDuty;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this, ToStringStyle.JSON_STYLE)
+                .append("name", name)
+                .append("id", id)
+                .append("isOnDuty", isOnDuty)
+                .toString();
+    }
+
+    private void logDebug(String message) {
+        System.out.println("[DEBUG] " + message + " | Current State: " + this.toString());
     }
 }
+
--- Classes/TriageLevel.java
+++ Classes/TriageLevel.java
@@ -1,31 +1,41 @@
-package Classes;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.Validate;
 
 public class TriageLevel {
     private int patientId;
     private String color;
-    
-    //Constructor
+
+    // Constructor
     public TriageLevel(int id, String color) {
+        Validate.isTrue(id > 0, "Patient ID must be positive.");
         this.patientId = id;
-        this.color = color;
+        this.color = StringUtils.defaultIfEmpty(color, "Undefined");
     }
 
-    //get - set
-    public int getPatientId(){
+    // Getters and Setters
+    public int getPatientId() {
         return patientId;
     }
 
-    public void setPatientId(int id){
+    public void setPatientId(int id) {
+        Validate.isTrue(id > 0, "Patient ID must be positive.");
         this.patientId = id;
     }
 
-    public String getColor(){
+    public String getColor() {
         return color;
     }
 
-    public void setColor(String color){
-        this.color = color;
-
+    public void setColor(String color) {
+        this.color = StringUtils.defaultIfEmpty(color, "Undefined");
     }
 
+    public boolean isValidColor() {
+        return StringUtils.equalsAnyIgnoreCase(color, "Red", "Yellow", "Green", "Blue");
+    }
+
+    @Override
+    public String toString() {
+        return String.format("TriageLevel{Patient ID=%d, Color='%s'}", patientId, color);
+    }
 }
